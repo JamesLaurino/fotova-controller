6aa96d716c1aef746d21df68892a271457540b76
jamesLaurino <petersthomas90@gmail.com>
2025-02-20 19:14:35 +0100

    FTO-0004 create find category by id

---
+++ src/main/java/com/fotova/firstapp/controller/category/CategoryController.java
@@ -0,0 +1,26 @@
+package com.fotova.firstapp.controller.category;
+
+import com.fotova.entity.CategoryEntity;
+import com.fotova.service.category.CategoryMapper;
+import com.fotova.service.category.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    @GetMapping("auth/category/{categoryId}")
+    public ResponseEntity<Object> getProductById(@PathVariable int categoryId) {
+
+        CategoryEntity categoryEntity = categoryService.getCategoryById(categoryId);
+        return ResponseEntity.ok(categoryMapper.toCategoryDto(categoryEntity));
+    }
+}

8a879af83fd4744f66a31cdfb9e3bc4463919d3e
jamesLaurino <petersthomas90@gmail.com>
2025-02-26 16:57:13 +0100

    FTEO-0014 add address

--- src/main/java/com/fotova/firstapp/controller/address/AddressController.java
+++ src/main/java/com/fotova/firstapp/controller/address/AddressController.java
@@ -1,12 +1,10 @@
 package com.fotova.firstapp.controller.address;
 
+import com.fotova.dto.address.AddressDto;
 import com.fotova.service.address.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1")
@@ -23,5 +21,10 @@ public class AddressController {
     @GetMapping("auth/address/{addressId}")
     public ResponseEntity<Object> getAddressById(@PathVariable Integer addressId) {
         return ResponseEntity.ok(addressService.getAddressById(addressId));
+    }
+
+    @PostMapping("auth/address/add")
+    public ResponseEntity<Object> addAddress(@RequestBody AddressDto addressDto) {
+        return ResponseEntity.ok(addressService.addAddress(addressDto));
     }
 }

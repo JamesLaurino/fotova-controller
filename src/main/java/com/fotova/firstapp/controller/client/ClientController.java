07b332125e14c93065d0704de3485a45ca3b207d
jamesLaurino <petersthomas90@gmail.com>
2025-03-04 15:19:21 +0100

    FTO-0028 retrieve user by id

--- src/main/java/com/fotova/firstapp/controller/client/ClientController.java
+++ src/main/java/com/fotova/firstapp/controller/client/ClientController.java
@@ -4,6 +4,7 @@ import com.fotova.service.client.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,10 @@ public class ClientController {
     @GetMapping("auth/clients")
     public ResponseEntity<Object> getAllClient() {
         return ResponseEntity.ok(clientService.getAllClients());
+    }
+
+    @GetMapping("auth/client/{clientId}")
+    public ResponseEntity<Object> getClientById(@PathVariable Integer clientId) {
+        return ResponseEntity.ok(clientService.getClientById(clientId));
     }
 }

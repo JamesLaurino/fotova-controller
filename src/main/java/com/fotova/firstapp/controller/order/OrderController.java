c99b3c875b7951590bef61aa8097ee8a285a89a1
jamesLaurino <petersthomas90@gmail.com>
2025-03-06 12:25:57 +0100

    FTO-0046 retreive all orders

---
+++ src/main/java/com/fotova/firstapp/controller/order/OrderController.java
@@ -0,0 +1,21 @@
+package com.fotova.firstapp.controller.order;
+
+import com.fotova.service.order.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("auth/orders")
+    public ResponseEntity<Object> getAllComments() {
+        return ResponseEntity.ok(orderService.getAllOrders());
+    }
+}

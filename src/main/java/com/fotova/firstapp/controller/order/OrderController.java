7fe8e04c98ab61635768e0dc13d6132f41e2e950
jamesLaurino <petersthomas90@gmail.com>
2025-03-06 15:41:24 +0100

    FTO-0048 add a order

--- src/main/java/com/fotova/firstapp/controller/order/OrderController.java
+++ src/main/java/com/fotova/firstapp/controller/order/OrderController.java
@@ -1,12 +1,10 @@
 package com.fotova.firstapp.controller.order;
 
+import com.fotova.dto.order.OrderDto;
 import com.fotova.service.order.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1")
@@ -23,5 +21,10 @@ public class OrderController {
     @GetMapping("auth/order/{orderId}")
     public ResponseEntity<Object> getOrderById(@PathVariable Integer orderId) {
         return ResponseEntity.ok(orderService.getOrderById(orderId));
+    }
+
+    @PostMapping("auth/order/add")
+    public ResponseEntity<Object> addOrder(@RequestBody OrderDto orderDto) {
+        return ResponseEntity.ok(orderService.addOrder(orderDto));
     }
 }

19da51957eac846f71673c1f82c658043b6a126e
jamesLaurino <petersthomas90@gmail.com>
2025-05-06 16:07:55 +0200

    FTO-TST-0070 Implement unit test for comment

---
+++ src/test/java/com/fotova/firstapp/repository/comment/CommentServiceIntegrateTest.java
@@ -0,0 +1,14 @@
+package com.fotova.firstapp.repository.comment;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceIntegrateTest {
+
+    // updateClientCommentId
+    // updateCommentClientId
+    // setCommentClientId
+}

64d2161a8ec2e86e4356ba62736055bc62b8261e
jamesLaurino <petersthomas90@gmail.com>
2025-08-14 12:22:04 +0200

    FTO-106 create contact rest controller

--- src/main/java/com/fotova/firstapp/controller/rabbit/RabbitController.java
+++ src/main/java/com/fotova/firstapp/controller/rabbit/RabbitController.java
@@ -1,8 +1,12 @@
 package com.fotova.firstapp.controller.rabbit;
 
 import com.drools.dto.product.ProductDtoDrl;
+import com.fotova.dto.contact.ContactDto;
+import com.fotova.firstapp.security.utils.Response;
 import com.fotova.service.product.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +15,16 @@ public class RabbitController {
     @Autowired
     private ProductService productService;
 
+    @PostMapping("auth/amq/contact")
+    public ResponseEntity<Object> sendEmailFromContact(@RequestBody ContactDto contactDto) {
+        Response<String> response = Response.<String>builder()
+                .responseCode(HttpStatus.OK.value())
+                .responseMessage("Mail send successfully")
+                .data(productService.sendMailFromContact(contactDto))
+                .success(true)
+                .build();
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("auth/amq/products")
     public String testAMQProduct(@RequestBody ProductDtoDrl productDto) {

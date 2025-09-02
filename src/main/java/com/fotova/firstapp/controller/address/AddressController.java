a83c436a9168acb0228ba44e9fa30154d27e0398
jamesLaurino <petersthomas90@gmail.com>
2025-09-02 11:49:37 +0200

    FTO-117 implement swagger doc

--- src/main/java/com/fotova/firstapp/controller/address/AddressController.java
+++ src/main/java/com/fotova/firstapp/controller/address/AddressController.java
@@ -3,6 +3,13 @@ package com.fotova.firstapp.controller.address;
 import com.fotova.dto.address.AddressDto;
 import com.fotova.firstapp.security.utils.Response;
 import com.fotova.service.address.AddressService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +19,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1")
+@OpenAPIDefinition(info = @Info
+        (title = "Fotova creation - Definition", version = "1.0",
+                description = "Operations that help manage fotova-creation" ))
 public class AddressController {
 
     @Autowired
     private AddressService addressService;
 
+    @Operation(summary = "Retrieve all the client address")
+    @ApiResponse(responseCode = "200", description = "Addresses retrieved successfully",
+    content = {
+        @Content(mediaType = "application/json",
+        schema = @Schema(implementation = AddressDto.class))
+    })
     @GetMapping("auth/address")
     public ResponseEntity<Object> getAllAddresses() {
         Response<List<AddressDto>> response = Response.<List<AddressDto>>builder()
@@ -29,7 +45,9 @@ public class AddressController {
     }
 
     @GetMapping("auth/address/{addressId}")
-    public ResponseEntity<Object> getAddressById(@PathVariable Integer addressId) {
+    public ResponseEntity<Object> getAddressById(
+            @Parameter(description = "Address identifier", required = true, example = "1")
+            @PathVariable Integer addressId) {
         Response<AddressDto> response = Response.<AddressDto>builder()
                 .responseCode(HttpStatus.OK.value())
                 .responseMessage("Address retrieved successfully")

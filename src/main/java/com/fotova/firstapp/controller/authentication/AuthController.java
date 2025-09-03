91b017bcce6a73e2721e7f63c149e0ab0612456d
jamesLaurino <petersthomas90@gmail.com>
2025-09-03 13:38:43 +0200

    FTO-117 implement swagger doc - authentication.

--- src/main/java/com/fotova/firstapp/controller/authentication/AuthController.java
+++ src/main/java/com/fotova/firstapp/controller/authentication/AuthController.java
@@ -1,11 +1,14 @@
 package com.fotova.firstapp.controller.authentication;
 
-import com.fotova.dto.authentification.AuthentificationDto;
 import com.fotova.dto.authentification.ResponseDto;
 import com.fotova.dto.request.LoginRequest;
 import com.fotova.dto.request.RegisterRequest;
 import com.fotova.dto.request.ResetPasswordRequest;
 import com.fotova.firstapp.security.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,41 +22,65 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @PostMapping("auth/login/demo")
-    public ResponseDto authDemoReact(@RequestBody AuthentificationDto authentication) {
 
-        ResponseDto responseDto = new ResponseDto();
-
-        if(authentication.getUsername().equals("admin") && authentication.getPassword().equals("admin")){
-            responseDto.setAuthenticate(true);
-            return responseDto;
-        }
-        else {
-            responseDto.setAuthenticate(false);
-            return responseDto;
-        }
-    }
-
+    @Operation(summary = "Register a new user")
+    @ApiResponse(responseCode = "200", description = "User registered successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ResponseDto.class))
+            })
+    @ApiResponse(responseCode = "400", description = "Username or email already registered",
+            content = @Content)
     @PostMapping(path = "auth/register")
     public ResponseEntity<Object> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
+    @Operation(summary = "Reset user password")
+    @ApiResponse(responseCode = "200", description = "Password reset successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ResponseDto.class))
+            })
     @PostMapping("auth/password-reset")
     public ResponseEntity<Object> resetPassword(@RequestBody ResetPasswordRequest request) {
         return ResponseEntity.ok(authService.resetPassword(request));
     }
 
+    @Operation(summary = "Check register email")
+    @ApiResponse(responseCode = "200", description = "Email checked successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ResponseDto.class))
+            })
+    @ApiResponse(responseCode = "500", description = "Invalid verification code",
+            content = @Content)
     @GetMapping("auth/register/check")
     public ResponseEntity<Object> checkRegisterEmail(@RequestParam String uuid) {
         return ResponseEntity.ok(authService.registerAfterEmailConfirm(uuid));
     }
 
+    @Operation(summary = "User login")
+    @ApiResponse(responseCode = "200", description = "User logged in successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ResponseDto.class))
+            })
+    @ApiResponse(responseCode = "404", description = "User not found",
+            content = @Content)
     @PostMapping("auth/login")
     public ResponseEntity<Object> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @Operation(summary = "Get current user")
+    @ApiResponse(responseCode = "200", description = "User retrieved successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ResponseDto.class))
+            })
+    @ApiResponse(responseCode = "404", description = "User not found",
+            content = @Content)
     @GetMapping("/user")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Object> getUser() {

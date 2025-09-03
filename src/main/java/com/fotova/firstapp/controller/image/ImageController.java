a43f16993c0c5d268e9e416d79d08e6c5d3d868e
jamesLaurino <petersthomas90@gmail.com>
2025-09-03 13:05:42 +0200

    FTO-117 implement swagger doc - image.

--- src/main/java/com/fotova/firstapp/controller/image/ImageController.java
+++ src/main/java/com/fotova/firstapp/controller/image/ImageController.java
@@ -3,6 +3,11 @@ package com.fotova.firstapp.controller.image;
 import com.fotova.dto.image.ImageDto;
 import com.fotova.firstapp.security.utils.Response;
 import com.fotova.service.image.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,12 @@ public class ImageController {
     @Autowired
     private ImageService imageService;
 
+    @Operation(summary = "Retrieve all images from database")
+    @ApiResponse(responseCode = "200", description = "Images retrieved successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = ImageDto.class))
+            })
     @GetMapping("auth/images")
     public ResponseEntity<Object> getAllImages() {
         Response<List<ImageDto>> response = Response.<List<ImageDto>>builder()
@@ -28,6 +39,22 @@ public class ImageController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Retrieve a images by id",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Image retrieved successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ImageDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Image not found for the given id",
+                            content = @Content()
+                    )
+            })
     @GetMapping("auth/image/{id}")
     public ResponseEntity<Object> getImageById(@PathVariable("id") Integer imageId) {
         Response<ImageDto> response = Response.<ImageDto>builder()
@@ -39,8 +66,26 @@ public class ImageController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Delete a image by his id",
+    responses = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "Image deleted successfully",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ImageDto.class)
+                )
+        ),
+        @ApiResponse(
+                responseCode = "409",
+                description = "Image not found for the given id",
+                content = @Content()
+        )
+    })
     @DeleteMapping("auth/image/{id}/delete")
-    public ResponseEntity<Object> deleteImage(@PathVariable("id") Integer imageId) {
+    public ResponseEntity<Object> deleteImage(
+            @Parameter(description = "image id", required = true, example = "1")
+            @PathVariable("id") Integer imageId) {
         Response<String> response = Response.<String>builder()
                 .responseCode(HttpStatus.OK.value())
                 .responseMessage("Images deleted successfully")
@@ -50,9 +95,22 @@ public class ImageController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Delete a image from product with product id",
+    responses = {
+        @ApiResponse(
+                responseCode = "200",
+                description = "Image deleted successfully",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ImageDto.class))
+        )
+    })
     @DeleteMapping("auth/image/delete/{imageName}/{productId}")
-    public ResponseEntity<Object> deleteImageFromProduct(@PathVariable("imageName") String imageName,
-                                                         @PathVariable("productId") Integer productId) {
+    public ResponseEntity<Object> deleteImageFromProduct(
+            @Parameter(description = "image name", required = true, example = "test.jpg")
+            @PathVariable("imageName") String imageName,
+            @Parameter(description = "product id", required = true, example = "1")
+            @PathVariable("productId") Integer productId) {
         Response<String> response = Response.<String>builder()
                 .responseCode(HttpStatus.OK.value())
                 .responseMessage("Images deleted successfully")
@@ -62,8 +120,22 @@ public class ImageController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Add a image with his id",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Image added successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ImageDto.class)
+                            )
+                    )
+            })
     @PostMapping("auth/image/add/{id}")
-    public ResponseEntity<Object> addImage(@RequestBody ImageDto imageDto, @PathVariable("id") Integer productId) {
+    public ResponseEntity<Object> addImage(
+            @RequestBody ImageDto imageDto,
+            @Parameter(description = "image id", required = true, example = "1")
+            @PathVariable("id") Integer productId) {
         Response<ImageDto> response = Response.<ImageDto>builder()
                 .responseCode(HttpStatus.OK.value())
                 .responseMessage("Image added successfully")
@@ -73,6 +145,22 @@ public class ImageController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Update a image",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Image updated successfully",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ImageDto.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Image not found for the given id",
+                            content = @Content()
+                    )
+            })
     @PutMapping("auth/image/update")
     public ResponseEntity<Object> updateImage(@RequestBody ImageDto imageDto) {
         Response<ImageDto> response = Response.<ImageDto>builder()

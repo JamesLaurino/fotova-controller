047412df32387fba80b2cd4bea7ff976f569fcaa
jamesLaurino <petersthomas90@gmail.com>
2025-09-03 13:14:29 +0200

    FTO-117 implement swagger doc - comment.

--- src/main/java/com/fotova/firstapp/controller/comment/CommentController.java
+++ src/main/java/com/fotova/firstapp/controller/comment/CommentController.java
@@ -4,6 +4,11 @@ import com.fotova.dto.client.ClientDto;
 import com.fotova.dto.comment.CommentDto;
 import com.fotova.firstapp.security.utils.Response;
 import com.fotova.service.comment.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,12 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+    @Operation(summary = "Retrieve all comments")
+    @ApiResponse(responseCode = "200", description = "Comments retrieved successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = CommentDto.class))
+            })
     @GetMapping("auth/comments")
     public ResponseEntity<Object> getAllComments() {
         Response<List<CommentDto>> response = Response.<List<CommentDto>>builder()
@@ -30,8 +41,18 @@ public class CommentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Retrieve a comment by its id")
+    @ApiResponse(responseCode = "200", description = "Comment retrieved successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = CommentDto.class))
+            })
+    @ApiResponse(responseCode = "404", description = "Comment not found",
+            content = @Content)
     @GetMapping("auth/comment/{commentId}")
-    public ResponseEntity<Object> getCommentById(@PathVariable("commentId") Integer commentId) {
+    public ResponseEntity<Object> getCommentById(
+            @Parameter(description = "Comment identifier - id", required = true, example = "1")
+            @PathVariable("commentId") Integer commentId) {
         Response<CommentDto> response = Response.<CommentDto>builder()
                 .responseCode(HttpStatus.OK.value())
                 .responseMessage("Comment retrieved successfully")
@@ -41,6 +62,14 @@ public class CommentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Update a comment")
+    @ApiResponse(responseCode = "200", description = "Comment updated successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = CommentDto.class))
+            })
+    @ApiResponse(responseCode = "404", description = "Comment not found",
+            content = @Content)
     @PutMapping("auth/comment/update")
     public ResponseEntity<Object> updateComment(@RequestBody CommentDto commentDto) {
         Response<CommentDto> response = Response.<CommentDto>builder()
@@ -52,6 +81,14 @@ public class CommentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Add a new comment")
+    @ApiResponse(responseCode = "200", description = "Comment added successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = CommentDto.class))
+            })
+    @ApiResponse(responseCode = "409", description = "Comment already exists",
+            content = @Content)
     @PostMapping("auth/comment/add")
     public ResponseEntity<Object> addComment(@RequestBody CommentDto commentDto) {
         Response<CommentDto> response = Response.<CommentDto>builder()
@@ -63,8 +100,18 @@ public class CommentController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Delete a comment by its id")
+    @ApiResponse(responseCode = "200", description = "Comment deleted successfully",
+            content = {
+                    @Content(mediaType = "application/json", schema =
+                    @Schema(implementation = String.class))
+            })
+    @ApiResponse(responseCode = "404", description = "Comment not found",
+            content = @Content)
     @DeleteMapping("auth/comment/{commentId}/delete")
-    public ResponseEntity<Object> deleteComment(@PathVariable("commentId") Integer commentId) {
+    public ResponseEntity<Object> deleteComment(
+            @Parameter(description = "Comment identifier - id", required = true, example = "1")
+            @PathVariable("commentId") Integer commentId) {
         Response<String> response = Response.<String>builder()
                 .responseCode(HttpStatus.OK.value())
                 .responseMessage("Comment deleted successfully")

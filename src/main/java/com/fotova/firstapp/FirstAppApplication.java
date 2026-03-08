b00b940761f3342de80deab4ee7c39b83b8e9bee
jamesLaurino <petersthomas90@gmail.com>
2026-03-08 15:10:00 +0100

    FTO-0131 fixing properties config

--- src/main/java/com/fotova/firstapp/FirstAppApplication.java
+++ src/main/java/com/fotova/firstapp/FirstAppApplication.java
@@ -1,7 +1,5 @@
 package com.fotova.firstapp;
 
-import com.fotova.service.product.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -25,11 +23,6 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
         "com.fotova.firstapp.security.service.user",
         "com.fotova.firstapp.security.service"})
 public class FirstAppApplication {
-
-    @Autowired
-    private ProductService productService;
-
-
     public static void main(String[] args)
     {
         SpringApplication.run(FirstAppApplication.class, args);
